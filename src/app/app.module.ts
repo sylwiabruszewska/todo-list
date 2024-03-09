10ffd87440313ed15afd2c3ac1f09c02890ea837
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-03-09 12:58:05 +0100

    tidy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
-import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [AppComponent, TodoListComponent, TodoItemComponent],
